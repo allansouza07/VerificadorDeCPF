6fd18502ed9e99d95305a58d61369792111a7b4b
Allan <allangomes_07@outlook.com>
2026-07-05 22:03:08 -0300

    CRIAÇÃO DO MENU DE SORTEIO DE CPF

--- cpf.py
+++ cpf.py
@@ -54,7 +54,7 @@ O segundo dígito do CPF é 0
 """
 # cpf = '36440847007'  # Esse CPF gera o primeiro dígito como 10 (0)
 
-
+import random
 ##FUNÇÃO QUE DESCOBRE OS ÚLTIMOS 2 DÍGITOS DO CPF
 def ultimos_dois ():
     cpf = None
@@ -188,6 +188,69 @@ def validador():
         print()
         print(f"O CPF {cpf} é inválido!")
    
+def aleatorio():
+    #DECLARAÇÃO DE VARIÁVEIS
+    digito_um = 0
+    quantidade = None
+    cpf_aleatorio = ''
+
+
+    while True:
+        #COLETA DA QUANTIDADE DE CPFS
+        try:
+            quantidade = int(input("Digite aqui quantos CPFs você deseja gerar: "))
+        except ValueError:
+            print("Digite número, não strings!")
+        else:
+            break
+        
+    #SORTEIO DOS 9 PRIMEIROS NÚMEROS DO CPF
+    for cont in range (0, quantidade):
+        cpf_aleatorio =''
+        for c in range (0, 9):
+            cpf_aleatorio += str(random.randint(0,9))
+
+        acumulador = 0
+        indice=-1
+        for c in range(10,1, -1):
+            indice +=1
+            #MULTIPLICANDO OS DIGITOS DO CPF EM UMA SEQUÊNCIA DE 10 DIGITOS
+            acumulador = acumulador + c * int(cpf_aleatorio[indice])
+
+        #MULTIPLICANDO A SOMA DESSES NÚMEROS POR 10 E PEGANDO O RESTO DA DIVISÃO POR 11
+        resto_divisao= (acumulador * 10) % 11
+
+        if resto_divisao > 9:
+            digito_um = 0
+        else :
+            digito_um = resto_divisao
+
+        cpf_completo = (f"{cpf_aleatorio[:9]}-{digito_um}" )
+
+
+        ##SEGUNDO DIGITO
+        cpf_nmr = [str(cpf_aleatorio[:9]) + str(digito_um) ]
+        segundo_digito = None
+
+        indice_2 = -1
+        acumulador_2 =0
+        for c in range (11,1, -1):
+            indice_2 +=1
+            #MULTIPLICANDO OS DIGITOS DO CPF EM UMA SEQUÊNCIA DE 10 DIGITOS
+            acumulador_2 = acumulador_2 + c * int(cpf_nmr[0][indice_2])
+
+        #MULTIPLICANDO A SOMA DESSES NÚMEROS POR 10 E PEGANDO O RESTO DA DIVISÃO POR 11     
+        resto_divisao_2 = (acumulador_2 * 10) % 11
+
+        if resto_divisao_2 > 9:
+            segundo_digito = 0
+        else :
+            segundo_digito= resto_divisao_2
+
+        cpf_completo = (f"{cpf_aleatorio[:9]}-{digito_um}{segundo_digito}" )
+
+        #PRINT DO CPF DO LAÇO DE REPETIÇÃO
+        print(f"Seu cpf completo n° {cont + 1}: {cpf_completo}")
 
 print("Escolha o que você deseja fazer:")
 while True:
@@ -201,8 +264,12 @@ while True:
             print("Digite um valor entre 1 e 3!")
             continue
         break
+
+#DECISÕES
 if escolha == 1:
     ultimos_dois()
 if escolha == 2:
     validador()
+if escolha == 3:
+    aleatorio()
 
